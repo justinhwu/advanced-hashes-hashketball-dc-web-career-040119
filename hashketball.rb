c0f332118e262811a947e413b178385026352e18
Justin Wu <justinhwu95@gmail.com>
2019-03-20 23:12:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -60,7 +60,7 @@ end
 
 def player_numbers(team)
   game_hash.values.each do |a|
-    if a[:team_name] == (team)
+    if a.has_value?(team) == (team)
     a[:players].map do |b|
       b[:number]
     end
